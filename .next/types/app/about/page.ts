d997ea978fa90e59e8c573f17e71616abb60b748
ssparshaa <ssparshaaa@gmail.com>
2025-08-13 14:57:09 +0545

    added animation

--- .next/types/app/about/page.ts
+++ .next/types/app/about/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/swetathapa/Downloads/sambandha/app/about/page.tsx
+// File: /Users/sparshashrestha/Downloads/sambandha/app/about/page.tsx
 import * as entry from '../../../../app/about/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
